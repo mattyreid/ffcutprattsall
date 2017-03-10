3cc9ce89178ddf8e36c681d740f77fc475267706
mattyreid1 <djlivedtv@gmail.com>
2017-03-10 21:54:20 +0000

    header change

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,10 @@ Rails.application.routes.draw do
   end
 
   resources :tags, only: [:show]
-
-  get "me/bookmarks" => "dashboards#bookmarks", as: :dashboard_bookmarks
-  get "top-stories" => "dashboards#top_stories", as: :top_stories
+  
+  get "/home" => "dashboards#show"
+  get "/capsule" => "dashboards#bookmarks", as: :dashboard_bookmarks
+  get "/explore" => "dashboards#top_stories", as: :top_stories
   get "me/stories/drafts" => "stories#drafts", as: :stories_drafts
   get "me/stories/public" => "stories#published", as: :stories_published
   get "search" => "search#show", as: :search
