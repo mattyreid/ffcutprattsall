079e27c1dff1339ca8fa40546257002309495acd
mattyreid1 <djlivedtv@gmail.com>
2017-04-07 02:38:37 +0000

    update2

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ Rails.application.routes.draw do
 
   resources :tags, only: [:show]
 
-  get "me/bookmarks" => "dashboards#bookmarks", as: :dashboard_bookmarks
-  get "top-stories" => "dashboards#top_stories", as: :top_stories
+  get "me/Capsule" => "dashboards#bookmarks", as: :dashboard_bookmarks
+  get "top-Posts" => "dashboards#top_stories", as: :top_stories
   get "me/stories/drafts" => "stories#drafts", as: :stories_drafts
   get "me/stories/public" => "stories#published", as: :stories_published
   get "search" => "search#show", as: :search
